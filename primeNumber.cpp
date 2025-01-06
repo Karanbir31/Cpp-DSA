90034274dd1bd2110a5148a7252736a525f3e667
karanbir31 <karan0031singh@gmail.com>
2025-01-06 16:50:46 +0530

    update prime number program

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int primeOrNot(int number){
-    for(int i =2; i < number/2; i++){
+    if(number == 0 || number == 1){
+        return 0;
+    }
+    for(int i =2; i < number/2 +1 ; i++){
         if(number%i == 0){
             return 0;
         }
@@ -11,12 +14,21 @@ int primeOrNot(int number){
 }
 int main(){
     int number;
-    cout<<"Enter the number to check prime or not :";
-    cin>>number;
-    if(primeOrNot(number)){
-        cout<<number<<" is a prime number\n";
-    }else{
-        cout<<number<<" is not a prime number\n";
-    }
+    char ch = 'y';
+    
+    do{
+        cout<<"Enter the number to check prime or not :";
+        cin>>number;
+        cout<<endl<<abs(number)<<endl;
+         if(primeOrNot(abs(number))){
+             cout<<number<<" is a prime number\n";
+         }else{
+           cout<<number<<" is not a prime number\n";
+         }
+
+        cout<<"Do you want to check another number (y/n) :";
+        cin>>ch;
+
+    }while ( ch == 'y' || ch == 'Y');
     return 0;
 }
