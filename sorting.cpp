caf01b40567347986097fd5f1b4f410be3e4d17a
karanbir31 <karan0031singh@gmail.com>
2025-01-08 19:20:44 +0530

    ADD Bubble sorting in sorting file

--- sorting.cpp
+++ sorting.cpp
@@ -11,21 +11,51 @@ void enterArrayData(int * arr, int arrSize = 0){
 }
 
 void DisplayArrayData(int * arr, int arrSize = 0){
- cout<<"\n\nGiven array is :";
-    for (int i = 0; i < arrSize; i++)
-    {
+ cout<<"Given array is :";
+    for (int i = 0; i < arrSize; i++) {
         cout<<arr[i]<<" ";
-        
     }
 }
 
-int main(){
-   // clrscr();
-    int arrSize = 10;
-    int arr[arrSize];
+void swapTwoNumber(int *a, int *b){
+    *a= *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
 
-    enterArrayData(arr, arrSize);
+void bubbleSort(int * arr, int arrSize = 0){
+    for(int i =0 ; i < arrSize ; i++){
+        for(int j = 0  ; j < arrSize - (i + 1) ; j++){
+            if(arr[j] > arr[j+1]){
+                swapTwoNumber(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(){
+   
+    int arrSize = 10;
+    int arr[arrSize] = {2, 5, 1, 8, 9, 7, 2, 3, 8, 1};
+
+   // enterArrayData(arr, arrSize);
+    cout<<" \n Before Sorting ";
+    DisplayArrayData(arr, arrSize);
+
+    bubbleSort(arr, arrSize);
+
+    cout<<"\n After the soring";
     DisplayArrayData(arr, arrSize);
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
