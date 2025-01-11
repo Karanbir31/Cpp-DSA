3d448b5cff3f166ce906bfa4c888b6a79c58e4c4
karanbir31 <karan0031singh@gmail.com>
2025-01-11 14:26:35 +0530

    smallest +ve missing number

---
+++ array5.cpp
@@ -0,0 +1,36 @@
+//smallest +ve missing number
+// 1<= N <= 10^6  |  -10^6 <= Ai <= 10^6
+
+#include<iostream>
+using namespace std;
+
+int smallestMissingNumber(int *arr, int size){
+    const int N = 1e6 +2;
+    bool check[N];
+
+    for(int i =0; i <= N; i++){
+        check[i] = false;
+    }
+
+    for (int i = 0; i < size ; i++){
+        if(arr[i] >= 0){
+            check[arr[i]] = true;
+        }
+    }
+
+    for(int i =0; i <= N; i++ ){
+        if(!check[i]){
+            return i;
+        }
+    }
+
+}
+
+int main(){
+    int size = 8;
+    int arr[size] = {0, -9, 1, 3, -4, 5, 2, 4};
+
+    cout<<"missing number is : "<<smallestMissingNumber(arr, size);
+
+    return 0;
+}
