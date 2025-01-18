c9f7ccb6dbfffd344e48ae39eadb49432dcc165c
karanbir31 <karan0031singh@gmail.com>
2025-01-18 20:45:02 +0530

    knapSack problem

--- recursion_knapsackProblem.cpp
+++ recursion_knapsackProblem.cpp
@@ -5,23 +5,23 @@
 using namespace std;
 
 int knapsack(int vl[], int wt[], int n, int kwt){
-    if(n == 0 || kwt == 0){
+    if(n < 0 || kwt == 0){
         return 0;
     }
-    if(wt[n] > kwt){
+    if(wt[n-1] > kwt){
         return knapsack(vl, wt, n-1, kwt);
         //currItem wt > reamning knapsack wt limit than skip move to next
     }
     return max(
-        knapsack(vl, wt, n-1, kwt),
-        (knapsack(vl, wt, n-1, kwt - wt[0]) + vl[0])
+        knapsack(vl , wt, n-1, kwt),
+        (knapsack(vl, wt, n-1, kwt - wt[n-1]) + vl[n-1])
     );
 }
 
 int main(){
     int n = 3;
     int wt[n] = {10, 20, 30};
-    int vl[n] = {80, 50, 120};
+    int vl[n] = {50, 50, 50};
     int kwt = 50;
 
     cout<< "max value of items that can store in knapsack is : " << knapsack(vl, wt, n, kwt);
