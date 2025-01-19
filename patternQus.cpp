e452b911ccb91ce20f329481f44ce090e2cf0a00
karanbir31 <karan0031singh@gmail.com>
2025-01-19 20:48:24 +0530

    pyramid patterns

--- patternQus.cpp
+++ patternQus.cpp
@@ -23,9 +23,52 @@ void hollowRectangle(int r, int c){
     }
 }
 
+void invertedHalfPyramid(int n = 5){
+    for(int i = n; i > 0; i--){
+        for(int j = 1; j <= i; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+void halfPyramidRotated(int n = 5){
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j > 0; j--){
+            if(i >= j)
+                cout<< " * ";
+            else
+                cout << " - " ;
+        }
+        cout<<endl;
+    }
+}
+void invertedHalfPyramidNumbers(int n = 5){
+    for(int i = n; i > 0; i--){
+        for(int j = 1; j <= i; j++){
+            cout<<" "<<n+1-i<<" ";
+        }
+        cout<<endl;
+    }
+}
+void halfPyramidRotatedNumber(int n = 5){
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j > 0; j--){
+            if(i >= j)
+                cout<<" "<<i<<" ";
+            else
+                cout << " - " ;
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
 
     //rectanglePattern(2,3);
-    hollowRectangle(5, 4);
+    //hollowRectangle(5, 4);
+    //invertedHalfPyramid();
+    invertedHalfPyramidNumbers();
+    cout<<endl;
+    halfPyramidRotatedNumber();
     return 0;
 }
