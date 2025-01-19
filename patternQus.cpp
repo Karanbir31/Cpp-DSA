965af3b2528f83962ac934265e3e94782a41a23f
karanbir31 <karan0031singh@gmail.com>
2025-01-19 20:25:42 +0530

    hollow rectangle pattern

--- patternQus.cpp
+++ patternQus.cpp
@@ -11,9 +11,21 @@ void rectanglePattern(int r, int c){
         cout<<endl;
     }
 }
+void hollowRectangle(int r, int c){
+    for(int i = 0; i < r; i++){
+        for(int j=0; j < c; j++){
+            if( i == 0 || i == r-1 || j == 0 || j == c-1)
+                cout<<" * ";
+            else 
+                cout<<"   ";
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
 
-    rectanglePattern(2,3);
+    //rectanglePattern(2,3);
+    hollowRectangle(5, 4);
     return 0;
 }
